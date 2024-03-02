10650956a0600e2f501a0e5cf40ee80833f31622
Mariany Silva <fsilvamariany@gmail.com>
2024-03-02 16:41:13 -0300

    fix(activity): add content description

--- db/migrations/000001_create_activities_table.up.sql
+++ db/migrations/000001_create_activities_table.up.sql
@@ -4,13 +4,13 @@ CREATE TABLE activities
 (
     id              integer         PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     uuid            uuid            DEFAULT uuid_generate_v4() NOT NULL,
-    content_uuid    uuid            NOT NULL,
-
+    content_uuid    uuid            NOT NULL, -- Main content of the activity. This content has a specific type
+                                              -- (video, pdf, questionnaire, slide, etc.), each type of content
+                                              -- is implemented in a dedicated Micro Service.
     name            varchar         NOT NULL,
     description     text            NULL,
     content_type    text            NOT NULL,
     taxonomy        text            NULL,
-
     created_at      timestamp       DEFAULT now() NOT NULL,
     updated_at      timestamp       DEFAULT now() NOT NULL,
     deleted_at      timestamp
